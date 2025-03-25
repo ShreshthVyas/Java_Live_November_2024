36198345f19d5a072259a9ee33023215cb21c808
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2025-03-25 21:02:23 +0530

    Map and Set - 1

--- Java_Live_November_2024/src/Lecture45/IntersectionOfTwoArray.java
+++ Java_Live_November_2024/src/Lecture45/IntersectionOfTwoArray.java
@@ -12,29 +12,27 @@ public class IntersectionOfTwoArray {
 	public int[] intersection(int[] nums1, int[] nums2) {
 		HashSet<Integer> set = new HashSet<>();
 		HashSet<Integer> ans = new HashSet<>();
-		
+
 		for (int i : nums1) {
 			set.add(i);
 		}
-		
+
 		for (int i : nums2) {
-			if(set.contains(i)) { // common element
+			if (set.contains(i)) { // common element
 				ans.add(i);
 			}
 		}
-		
+
 		int arr[] = new int[ans.size()];
 		int idx = 0;
-		
+
 		for (int i : ans) {
 			arr[idx] = i;
 			idx++;
 		}
-		
+
 		return arr;
-		
-		
-		
+
 	}
 
 }
